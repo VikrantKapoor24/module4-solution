e3f40f20e0b84293d6376a4616075aef41dcab1e
VikrantKapoor24 <86543354+VikrantKapoor24@users.noreply.github.com>
2021-07-02 09:49:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -60,3 +60,20 @@ for (/* fill in parts of the 'for' loop to loop over names array */) {
     // helloSpeaker.xxxx
   }
 }
+
+(function () {
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+for (var i = 0; i < names.length; i++) {
+  var firstLetter = names[i].charAt(0).toLowerCase();
+
+  if (firstLetter === 'j') {
+    byeSpeaker(names[i]);
+  }
+  else {
+    helloSpeaker(names[i]);
+  }
+}
+
+})();
